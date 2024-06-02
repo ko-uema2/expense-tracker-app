3716ad8271b38c724f6962e14b7bbeb7818ee242
ko-uema2 <uematsuku@gmail.com>
2024-06-02 22:42:44 +0900

    update dynamoDBとS3Bucketのリソース定義を分割

---
+++ back/lib/storage/dynamo.ts
@@ -0,0 +1,39 @@
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export class ExpenseDataTable extends Construct {
+  readonly table: dynamodb.TableV2;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // Create the DynamoDB table for expense data
+    this.table = new dynamodb.TableV2(this, "ExpenseDataTable", {
+      tableName: "ExpenseData",
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "expenseDate",
+        type: dynamodb.AttributeType.STRING,
+      },
+      globalSecondaryIndexes: [
+        {
+          indexName: "expenseDataByUser",
+          partitionKey: {
+            name: "userId",
+            type: dynamodb.AttributeType.STRING,
+          },
+          sortKey: {
+            name: "expenseDate",
+            type: dynamodb.AttributeType.STRING,
+          },
+        },
+      ],
+      billing: dynamodb.Billing.onDemand(),
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+  }
+}
