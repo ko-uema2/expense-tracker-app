74190ec91d78a1ce3756cf7bb7e9e8a4a81bfe73
ko-uema2 <uematsuku@gmail.com>
2024-08-05 21:13:42 +0900

    improve AppSync に引き渡す IAM ロールから LambdaExecutionRole を削除し、自前のポリシーを付与

--- back/lib/gateway/appsync.ts
+++ back/lib/gateway/appsync.ts
@@ -1,3 +1,5 @@
+import * as iam from "aws-cdk-lib/aws-iam";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
 import * as appsync from "aws-cdk-lib/aws-appsync";
 import { Construct } from "constructs";
 import path = require("path");
@@ -19,6 +21,11 @@ export class AppSync extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    // Create an IAM role for the AppSync API
+    const appsyncRole = new iam.Role(this, "appsyncRole", {
+      assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
+    });
+
     // Create the GraphQL API for the expense tracker
     this.api = new appsync.GraphqlApi(this, id, {
       name: id,
@@ -30,6 +37,23 @@ export class AppSync extends Construct {
           authorizationType: appsync.AuthorizationType.IAM,
         },
       },
+      logConfig: {
+        fieldLogLevel: appsync.FieldLogLevel.ALL,
+        role: appsyncRole,
+      },
     });
+
+    // Add necessary permissions to the AppSync API execution role
+    appsyncRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: [this.api.logGroup.logGroupArn],
+      })
+    );
   }
 }
